2a7f4abd5ea6de8f6121ad96535d585333885823
Tunisia Artope <tartope@xpanxion.com>
2023-07-06 20:53:24 -0400

    Complete assignment.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,33 @@
 import React, { useEffect, useState } from 'react'
 
-function index() {
+function Index() {
+  const [message, setMessage] = useState("Loading");
+  const [people, setPeople] = useState([]);
+
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/api/home")
+    .then(res => res.json())
+    .then(data =>{
+      // console.log(data)
+      setMessage(data.message)
+      // console.log(data.people)
+      setPeople(data.people)
+    })
+  }, [])
+
   return (
-    <div>index</div>
+    <div>
+      <div>{message}</div>
+      <div>
+        {
+          people.map((person, index)=>(
+            <div key={index}>{person}</div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
-export default index
+export default Index
--- server/server.py
+++ server/server.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 # Create app instance to create flask app
 app = Flask(__name__)
+# Set-up cors to allow next.js to make requests to the api
+CORS(app)
 
 
 # Create app route
 @app.route("/api/home", methods=['GET'])
 def return_home():
     return jsonify({
-        'message': "Hello world!"
+        'message': "Hello world!",
+        'people': ['Jack', 'Harry', 'Barry']
     })
 
 
@@ -16,4 +20,4 @@ def return_home():
 # Create a way to run the app
 if __name__ == '__main__':
     # Lets the file know you're in development mode.  Only remove 'debug=True' if you will deploy the app into production.
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
